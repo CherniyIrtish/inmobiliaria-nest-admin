a190ea13c1340e54b28291e64cd325f60028b5fd
com <>
2025-09-08 19:58:04 +0200

    update infra

---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "bucket_name" {
+  value = aws_s3_bucket.site.bucket
+}
+
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "distribution_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
+
+output "website_url" {
+  value = "https://${aws_cloudfront_distribution.cdn.domain_name}"
+}
